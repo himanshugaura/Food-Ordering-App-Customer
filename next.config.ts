638dfe1493d11356d1f405b6136284e48951ce93
Himanshu Gaura <himanshu32505@gmail.com>
2025-10-22 14:50:54 +0530

    updated : docker and cofig

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,12 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "robohash.org",
+        pathname: "/**",
       },
     ],
   },
